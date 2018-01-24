e7cc35f027227f1a914534ab2431e1d139ffbd4c
KrishnaP <kpat132@gmail.com>
2018-01-23 15:05:40 -1000

    functional now working on strech goals

--- client.js
+++ client.js
@@ -1,1 +1,18 @@
 const net = require('net');
+const client = new net.connect({ port: 6969, host: '0.0.0.0' }, () => {
+
+  console.log("Connected");
+
+  process.stdin.pipe(client);
+  client.pipe(process.stdout);
+
+});
+
+client.on('data', (data) => {
+  let info = data.toString();
+  //console.log(info);
+});
+
+client.on('end', () => {
+  console.log('Disconnected from server')
+});
--- server.js
+++ server.js
@@ -1,1 +1,40 @@
 const net = require('net');
+let information = [];
+
+const server = net.createServer(function (socket) {
+ // socket.write('Client logged on');
+
+  socket.setEncoding('utf8');
+
+  information.push(socket);
+
+  socket.write("Enter Username: ");
+
+  
+
+  socket.on('data', function (data) {
+    let info = data.toString();
+    console.log(info);
+
+    information.filter(function(element){
+      return element !== socket;
+
+    }).forEach(function(element, index){
+      // let first = information.indexOf(socket);
+      // console.log("CONSOLE: " + element[0]);
+      element.write(data);
+    });
+
+  });
+
+  socket.on('end', function () {
+    // information.splice(information.indexOf(socket,1));
+    console.log('client disconnected');
+
+  });
+  
+});
+
+server.listen(6969, '0.0.0.0', function () {
+  console.log('Server on');
+});
